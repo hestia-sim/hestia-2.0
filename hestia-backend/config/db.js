7fd8e17f21c2f659092371f62e7696be49ceabe2
Artur Mota <artur19rodriguesmota@gmail.com>
2026-01-19 10:56:23 -0300

    feat: add docker

--- hestia-backend/config/db.js
+++ hestia-backend/config/db.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log("--> HOST DO BANCO:", process.env.DB_HOST);
+console.log("--> USER DO BANCO:", process.env.DB_USER);
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
