ca0453f8c8278bcaf1b4234d438cb559af3179df
Artur Mota <artur19rodriguesmota@gmail.com>
2026-02-18 11:22:01 -0300

    feat: change info screen

--- hestia-frontend/src/pages/HowToUse/HowToUse.jsx
+++ hestia-frontend/src/pages/HowToUse/HowToUse.jsx
@@ -2,7 +2,6 @@
 import Header from "../../basics/Header/Header";
 // Images
 // Imports
-import { Accordion, AccordionTab } from "primereact/accordion";
 import { useTranslation } from "react-i18next";
 //Styles
 import s from "./HowToUse.module.scss";
@@ -18,91 +17,99 @@ export default function HowToUse() {
         <title>HESTIA | How To Use</title>
       </Helmet>
       <Header />
+
       <section className={s.hestiaInfoWrapper}>
         <h1>{t("howToUse")}</h1>
-        <div className={s.wrapperInternForm}>
-          <Accordion>
-            <AccordionTab header={t("howToUsePurpose")}>
-              <p className="m-0">
-                {t("howToUsePurposeDesc")}
-              </p>
-            </AccordionTab>
-            <AccordionTab header={t("howToUseHowWorks")}>
-              <p className="m-0">
-                {t("howToUseHowWorksDesc")}
-              </p>
-            </AccordionTab>
-            <AccordionTab header={t("howToUseParamsPresets")}>
-              <Accordion>
-                <AccordionTab header={t("howToUseParams")}>
-                  <p>
-                    {t("howToUseParamsDesc")}
-                  </p>
-                  <ul>
-                    <li>
-                      <strong>{t("howToUseParamsPeople").split(":")[0]}:</strong> {t("howToUseParamsPeople").split(":")[1]}
-                    </li>
-                    <li>
-                      <strong>{t("howToUseParamsActivities").split(":")[0]}:</strong> {t("howToUseParamsActivities").split(":")[1]}
-                    </li>
-                    <li>
-                      <strong>{t("howToUseParamsRooms").split(":")[0]}:</strong> {t("howToUseParamsRooms").split(":")[1]}
-                    </li>
-                    <li>
-                      <strong>{t("howToUseParamsActuators").split(":")[0]}:</strong> {t("howToUseParamsActuators").split(":")[1]}
-                    </li>
-                    <li>
-                      <strong>{t("howToUseParamsActivityPreset").split(":")[0]}:</strong> {t("howToUseParamsActivityPreset").split(":")[1]}
-                    </li>
-                  </ul>
-                </AccordionTab>
-                <AccordionTab header={t("howToUsePresets")}>
-                  <p className="m-0">
-                    {t("howToUsePresetsDesc")}
-                  </p>
-                </AccordionTab>
-              </Accordion>
-            </AccordionTab>
-            <AccordionTab header={t("howToUseStepByStep")}>
-              <Accordion>
-                <AccordionTab header={t("howToUseStep1")}>
-                  <p className="m-0">
-                    {t("howToUseStep1Desc")}
-                  </p>
-                </AccordionTab>
-                <AccordionTab header={t("howToUseStep2")}>
-                  <p className="m-0">
-                    {t("howToUseStep2Desc")}
-                  </p>
-                </AccordionTab>
-                <AccordionTab header={t("howToUseStep3")}>
-                  <p className="m-0">
-                    {t("howToUseStep3Desc")}
-                  </p>
-                </AccordionTab>
-                <AccordionTab header={t("howToUseStep4")}>
-                  <p className="m-0">
-                    {t("howToUseStep4Desc")}
-                  </p>
-                </AccordionTab>
-                <AccordionTab header={t("howToUseStep5")}>
-                  <p className="m-0">
-                    {t("howToUseStep5Desc")}
-                  </p>
-                </AccordionTab>
-                <AccordionTab header={t("howToUseStep6")}>
-                  <p className="m-0">
-                    {t("howToUseStep6Desc")}
-                  </p>
-                </AccordionTab>
-                <AccordionTab header={t("howToUseStep7")}>
-                  <p className="m-0">
-                    {t("howToUseStep7Desc")}
-                  </p>
-                </AccordionTab>
-              </Accordion>
-            </AccordionTab>
-          </Accordion>
+
+        <div className={s.contentFlow}>
+          <article className={s.stepSection}>
+            <h2>{t("howToUsePurpose")}</h2>
+            <p>{t("howToUsePurposeDesc")}</p>
+            <div className={s.imagePlaceholder}>
+               <img src="/info1.png"/>
+            </div>
+          </article>
+
+          <article className={s.stepSection}>
+            <h2>{t("howToUseHowWorks")}</h2>
+            <p>{t("howToUseHowWorksDesc")}</p>
+            <div className={s.imagePlaceholder}>
+               <img src="/info2.png"/>
+            </div>
+          </article>
+          <article className={s.stepSection}>
+            <h2>{t("howToUseParamsPresets")}</h2>
+            
+            <div className={s.subStep}>
+              <h3>{t("howToUseParams")}</h3>
+              <p>{t("howToUseParamsDesc")}</p>
+              <ul className={s.paramsList}>
+                <li><strong>{t("howToUseParamsPeople").split(":")[0]}:</strong> {t("howToUseParamsPeople").split(":")[1]}</li>
+                <li><strong>{t("howToUseParamsActivities").split(":")[0]}:</strong> {t("howToUseParamsActivities").split(":")[1]}</li>
+                <li><strong>{t("howToUseParamsRooms").split(":")[0]}:</strong> {t("howToUseParamsRooms").split(":")[1]}</li>
+                <li><strong>{t("howToUseParamsActuators").split(":")[0]}:</strong> {t("howToUseParamsActuators").split(":")[1]}</li>
+                <li><strong>{t("howToUseParamsActivityPreset").split(":")[0]}:</strong> {t("howToUseParamsActivityPreset").split(":")[1]}</li>
+              </ul>
+            </div>
+
+            <div className={s.imagePlaceholder}>
+               <img src="/info3.png"/>
+            </div>
+
+            <div className={s.subStep}>
+              <h3>{t("howToUsePresets")}</h3>
+              <p>{t("howToUsePresetsDesc")}</p>
+            </div>
+          </article>
+
+          <hr className={s.divider} />
+
+          <article className={s.stepSection}>
+            <h2>{t("howToUseStepByStep")}</h2>
+            
+            <div className={s.visualStep}>
+              <div className={s.stepText}>
+                <span>1</span>
+                <p>{t("howToUseStep1Desc")}</p>
+              </div>
+              <div className={s.imagePlaceholderSmall}>
+                <img src="/info4.png"/>
+              </div>
+            </div>
+
+            <div className={s.visualStep}>
+              <div className={s.stepText}>
+                <span>2</span>
+                <p>{t("howToUseStep2Desc")}</p>
+              </div>
+              <div className={s.imagePlaceholderSmall}>
+                <img src="/info5.png"/>
+              </div>
+            </div>
+
+            <div className={s.visualStep}>
+              <div className={s.stepText}>
+                <span>3</span>
+                <p>{t("howToUseStep3Desc")}</p>
+              </div>
+              <div className={s.imagePlaceholderSmall}>
+                <img src="/info6.png"/>
+              </div>
+            </div>
+
+            {/* Repita o padrão acima para os passos 4, 5, 6 e 7 conforme sua necessidade */}
+            {[4, 5, 6, 7].map((step) => (
+              <div key={step} className={s.visualStep}>
+                <div className={s.stepText}>
+                  <span>{step}</span>
+                  <p>{t(`howToUseStep${step}Desc`)}</p>
+                </div>
+                <div className={s.imagePlaceholderSmall}>
+                  <img src={`/info${step + 3}.png`}/>
+                </div>
+              </div>
+            ))}
+          </article>
         </div>
       </section>
     </main>
